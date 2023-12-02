b6fb667eae77e6cf8e295efa3a06b9a928418069
Chey <ctenmanikdavid@gmail.com>
2023-12-02 14:22:38 +0000

    Added React-Bootstrap navbar component and integrated Font Awesome icons for navigation.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import './App.module.css';
+import NavBar from './components/NavBar';
 
 function App() {
 	return (
 		<div className="App">
-			<Button variant="primary">Primary</Button>
+			<NavBar />
 		</div>
 	);
 }
