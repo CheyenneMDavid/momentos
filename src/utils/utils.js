acec98f02f4c8ca0d156e5744ce087fa62b16621
Chey <ctenmanikdavid@gmail.com>
2024-01-02 01:20:17 +0000

    Added localStorage handling for refresh token expiration in utils.js and removed StrictMode from index.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 import { axiosReq } from '../api/axiosDefaults';
+import jwtDecode from 'jwt-decode';
 
 export const fetchMoreData = async (resource, setResource) => {
 	try {
@@ -80,4 +81,42 @@ export const unfollowHelper = (profile, clickedProfile) => {
 		? { ...profile, following_count: profile.following_count - 1 }
 		: // Else the profile if returned unchanged.
 		  profile;
+};
+
+// ------ Cleaning up errors ----
+
+// The below functions collectively manage the refresh token's lifecycle in the browser's
+// local storage during the user's sessions in a web application.
+
+// This function is responsible for setting a timestamp in the browser's local storage that indicates
+// when the refresh token is expected to expire. It's useful for knowing when to attempt to refresh
+// the authentication token without making extra network requests
+export const setTokenTimestamp = (data) => {
+	// It uses the jwt-decode library to decode the JWT (JSON Web Token) and extract the expiration time (exp).
+	// The 'data?.refresh_token' is using optional chaining to ensure that if data is null or undefined,
+	// it doesn't try to access 'refresh_token' and cause a TypeError.
+
+	const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+
+	// The expiration time is then stored in the browser's local storage under the key 'refreshTokenTimestamp'.
+	// Local storage is a key-value store that allows me to save string data across browser sessions.
+	localStorage.setItem('refreshTokenTimestamp', refreshTokenTimestamp);
+};
+
+// This function checks if the timestamp for the refresh token exists in local storage, which
+// means that the user's still logged in and has a valid session.
+export const shouldRefreshToken = () => {
+	// It retrieves the timestamp using 'localStorage.getItem'. Then, the double NOT operator '!!'
+	// is used to convert the retrieved value to a boolean. If there's a timestamp present,
+	// it will return true, indicating that we should consider refreshing the token.
+	// If not, it returns false, indicating that there's no need to refresh the token.
+	return !!localStorage.getItem('refreshTokenTimestamp');
+};
+
+// This function is used to remove the stored refresh token timestamp from the browser's local storage.
+// Use it when the token's expired or the user logs out.
+export const removeTokenTimestamp = () => {
+	// The 'localStorage.removeItem' method removes the specified item from local storage,
+	// which means that the stored timestamp and any associated expiration information is cleared.
+	localStorage.removeItem('refreshTokenTimestamp');
 };
